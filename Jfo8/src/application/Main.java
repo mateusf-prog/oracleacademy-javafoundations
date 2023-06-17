46baaa8628503591770b464e8ff90aa7794cef91
mateusf-prog <mateus102006@hotmail.com>
2023-06-17 10:19:02 -0300

    updated main

--- Jfo8/src/application/Main.java
+++ Jfo8/src/application/Main.java
@@ -76,7 +76,7 @@ public class Main {
         }
         System.out.println();
         System.out.println();
-        //
+        
         try {
             System.out.println("--------MATCHES RESULTS------");
             System.out.println(league.matchHistory());
